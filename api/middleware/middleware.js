ce22020a940056b986e2f0a1bea5d7cb25f3c939
Sharon Mosher <sharon.a.mosher@gmail.com>
2021-03-16 19:38:51 -0400

    MVP

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,58 @@
+const Post = require("../posts/posts-model");
+const Users = require("../users/users-model");
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  console.log(
+    ` METHOD: ${req.method} URL: ${req.url} TIMESTAMP: ${Date.now()}`
+  );
+  next();
 }
 
-function validateUserId(req, res, next) {
+const validateUserId = async (req, res) => {
   // DO YOUR MAGIC
-}
+  const { id } = req.params;
+  try {
+    const user = await Post.getById(id);
+    if (!user) {
+      res.status(404).json({ message: `ID ${id} found.` });
+    } else {
+      res.status(200).json(user);
+    }
+  } catch (e) {
+    res.status(500).json({ message: `${e}` });
+  }
 
-function validateUser(req, res, next) {
+  const validateUser = async (req, res, next) => {
   // DO YOUR MAGIC
-}
+    const { id } = req.params;
+    try {
+      const user = await Users.getById(id);
+      if (!user) {
+        res.status(400).json({ message: `ID ${id} found.` });
+      } else {
+        next();
+      }
+    } catch (e) {
+      res.status(500).json({ message: `${e}` });
+    }
+  };
 
-function validatePost(req, res, next) {
+  function validatePost(req, res, next) {
   // DO YOUR MAGIC
-}
+    const { name, text, postedBy } = req.body;
+    if (!name || !text || !postedBy) {
+      res.status(404).json({ message: "You have to add a name to post!" });
+    } else {
+      next();
+    }
+  }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+}
+};
